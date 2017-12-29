69a96cdd4bc5de536dccf4599ac089c5886b2371
Chase Adams <chaseme37@gmail.com>
2017-12-28 22:25:14 -0700

    trying to big fix

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name: "Trevor",
+        photo: "https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/11146327_10206258907769497_5035769316691755025_o.jpg?oh=e73e68f5a6b9a88e96401da1a16453cb&oe=5AB5B0D2",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Tyson",
+        photo: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/10411001_448671651962673_8979622752919264263_n.jpg?oh=532ccb6e6ce7a402f9b4e910e94fd6d6&oe=5AB39EC5",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "5",
+            "4",
+            "1",
+            "2",
+            "1",
+            "5",
+            "4"
+        ]
+    },
+    {
+        name: "Scott",
+        photo: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/1002371_10153753321273383_2957309038086248763_n.jpg?oh=f513ddfc750ef70598680f9d0d8d91c1&oe=5AC09C7C",
+        scores: [
+            "3",
+            "3",
+            "1",
+            "5",
+            "4",
+            "2",
+            "5",
+            "3",
+            "3",
+            "5"
+        ]
+    },
+]
+
+module.exports = friends;
